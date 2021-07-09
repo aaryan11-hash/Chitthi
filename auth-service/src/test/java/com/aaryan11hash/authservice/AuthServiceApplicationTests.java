5ebe0f4408153eefc46d51ee51d6f70fd3933455
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-07-10 00:42:55 +0530

    19th commit<auth-service>[ADD] initialization of auth-service app

---
+++ auth-service/src/test/java/com/aaryan11hash/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaryan11hash.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
